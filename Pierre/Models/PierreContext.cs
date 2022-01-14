3aff67371cb1137bfe6223e9d0bf97d1c9426c27
Matt Luker <jamesmattluker@gmail.com>
2022-01-14 10:25:46 -0700

    add Home Index View and list treats and flavors

--- Pierre/Models/PierreContext.cs
+++ Pierre/Models/PierreContext.cs
@@ -7,7 +7,7 @@ namespace Pierre.Models
   {
     public DbSet<Treat> Treats { get; set; }
     public DbSet<Flavor> Flavors { get; set; }
-    public DbSet<TreatFlavor> TreatFlavor { get; set; }
+    public DbSet<FlavorTreat> FlavorTreat { get; set; }
 
     public PierreContext(DbContextOptions options) : base(options) { }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
